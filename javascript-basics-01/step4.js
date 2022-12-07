fb58dbd1c4bda9bd2cf68b341c4f6c9fff62b06d
rayan <rayanfayadhussein98@gmail.com>
2022-12-07 11:08:16 +0200

    step4

---
+++ javascript-basics-01/step4.js
@@ -0,0 +1,4 @@
+var name= prompt("what is your name");
+var username= prompt("what is your surname");
+var city= prompt("what is your city");
+alert("Nom : " + name +"\n" +"Prenom : " + username +"\n"+ "Ville : " + city);
