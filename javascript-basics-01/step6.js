0f72422bfad651112661c3788031235017609ef6
rayan <rayanfayadhussein98@gmail.com>
2022-12-08 20:37:24 +0200

    basics1_6update

--- javascript-basics-01/step6.js
+++ javascript-basics-01/step6.js
@@ -1,3 +1,8 @@
-var n1= prompt("what is the number1");
-var n2= prompt("what is the number2");
-alert(n1 % n2);
+
+var x= document.getElementById("validate");
+
+x.onclick = function divide(a, b) {
+  var a = parseInt(document.getElementById("first_number").value);
+  var b = parseInt(document.getElementById("second_number").value);
+  return alert(a / b);
+};
