6df305185bbea9cb70dba27924c33212d8dd154e
rayan <rayanfayadhussein98@gmail.com>
2022-12-08 21:05:29 +0200

    basics2_3

--- javascript-basics-02/step 3/step_3.js
+++ javascript-basics-02/step 3/step_3.js
@@ -1,1 +1,11 @@
-window.alert("hello")
+var yourName = document.querySelector("#name");
+var div = document.createElement("div");
+div.setAttribute("id", "container");
+document.body.appendChild(div);
+
+var container = document.getElementById("container");
+
+yourName.addEventListener("change", function () {
+  container.innerHTML = yourName.value;
+  
+});
