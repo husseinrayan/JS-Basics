c621ff19affca2fc3b0b62e3f71fb507c729b23d
rayan <rayanfayadhussein98@gmail.com>
2022-12-08 20:32:41 +0200

    basics1_4update

--- javascript-basics-01/step4.js
+++ javascript-basics-01/step4.js
@@ -1,4 +1,12 @@
-var name= prompt("what is your name");
-var username= prompt("what is your surname");
-var city= prompt("what is your city");
-alert("Nom : " + name +"\n" +"Prenom : " + username +"\n"+ "Ville : " + city);
+// var name= prompt("what is your name");
+// var username= prompt("what is your surname");
+// var city= prompt("what is your city");
+// alert("Nom : " + name +"\n" +"Prenom : " + username +"\n"+ "Ville : " + city);
+var button = document.getElementById("validate");
+
+button.onclick = function alertTest() {
+  userName = document.getElementById("name").value;
+  surname = document.getElementById("surname").value;
+  city = document.getElementById("city").value;
+
+  alert("Name: " + userName + "\n" + "Surname: " + surname + "\n" + "City: " + city);};
--- javascript-basics-01/step5.js
+++ javascript-basics-01/step5.js
@@ -1,3 +1,5 @@
 var n1= prompt("what is the number1");
 var n2= prompt("what is the number2");
+document.getElementById(first_number).value=n1;
+document.getElementById(second_number).value=n2;
 alert(n1 * n2);
